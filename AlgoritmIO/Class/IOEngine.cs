59d8f53d39e2649c0348657b7465d7e5a3692e1c
LittleDodjo <wake333@bk.ru>
2019-11-12 16:46:24 +0700

    #00000 created base classes

---
+++ AlgoritmIO/Class/IOEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmIO.Class
+{
+
+    /// <summary>
+    /// Базовый движок для работы с приложением
+    /// </summary>
+    public class IOEngine
+    {
+
+        #region CONSTANTS
+
+        public const string CONFIG_DIR = "config/"; // Базовый путь конфигурации
+        public const string DATA_DIR = "data/"; // Базовый путь данных
+        public const string CLASS_DIR = "bin/"; // Базоввый путь сериализованных классов
+
+        #endregion
+
+        /// <summary>
+        /// Экземпляр движка
+        /// </summary>
+        static private IOEngine Instance;
+        /// <summary>
+        /// Конфигурация движка
+        /// </summary>
+        private List<object> config;
+
+
+        /// <summary>
+        /// Получить экземпляр движка
+        /// </summary>
+        /// <returns></returns>
+        public static IOEngine GetInstance()
+        {
+            return Instance;
+        }
+    }
+}
---
+++ AlgoritmIO/Class/View/ViewAbstract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmIO.Class.View
+{
+    public abstract class ViewAbstract
+    {
+
+
+        private Dictionary<string, object> config;
+        /// <summary>
+        /// Конфигурация отображения
+        /// </summary>
+        public Dictionary<string, object> Config { get { return config; } set { config = value; } }
+
+
+    }
+}
